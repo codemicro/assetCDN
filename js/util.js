e4eb9fbf751b5105355da9f0929b72c0b68de71a
Tom <thomasdpain@gmail.com>
2019-07-10 18:34:46 +0100

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,3 +1,10 @@
+/* 
+ * I'm fairly sure this is part of my homepage template.
+ * Read Only by HTML5 UP
+ * html5up.net | @ajlkn
+ * Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+ */
+
 (function($) {
 
 	/**
